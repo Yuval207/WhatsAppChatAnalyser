562bbfb88ae246f84e9fc4d3cc18931acd9c857a
Vardaan Aggarwal <98269336+VardaanAggarwal@users.noreply.github.com>
2023-09-12 23:32:36 +0530

    fixed year bug in preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -6,15 +6,22 @@ def preprocess(data):
 
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
-
+    
     df = pd.DataFrame({'user_message': messages, 'message_date': dates})
-    # convert message_date type
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
+
+
+    try:
+        df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %H:%M - ')
+    except ValueError:
+        df['message_date'] = pd.NaT
+
+    df.dropna(subset=['message_date'], inplace=True)
 
     df.rename(columns={'message_date': 'date'}, inplace=True)
 
     users = []
     messages = []
+
     for message in df['user_message']:
         entry = re.split('([\w\W]+?):\s', message)
         if entry[1:]:  # user name
@@ -31,14 +38,15 @@ def preprocess(data):
     df['only_date'] = df['date'].dt.date
     df['year'] = df['date'].dt.year
     df['month_num'] = df['date'].dt.month
-    df['month'] = df['date'].dt.month_name()
+    df['month'] = df['date'].dt.strftime('%B')  # Use strftime to get the month name
     df['day'] = df['date'].dt.day
-    df['day_name'] = df['date'].dt.day_name()
+    df['day_name'] = df['date'].dt.strftime('%A')  # Use strftime to get the day name
     df['hour'] = df['date'].dt.hour
     df['minute'] = df['date'].dt.minute
 
     period = []
-    for hour in df[['day_name', 'hour']]['hour']:
+
+    for hour in df['hour']:
         if hour == 23:
             period.append(str(hour) + "-" + str('00'))
         elif hour == 0:
